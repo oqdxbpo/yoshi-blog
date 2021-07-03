b8e171a546daac627ca637508974dd62e4bc6e09
yoshi <hiryu1974@yahoo.co.jp>
2021-07-03 17:18:22 +0900

    フッター更新

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,9 +25,9 @@ const Layout = ({ location, title, children }) => {
       <header className="global-header">{header}</header>
       <main>{children}</main>
       <footer>
-        © {new Date().getFullYear()}, Built with
+        © {new Date().getFullYear()}, written by
         {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
+        <a href="https://twitter.com/yoshi19920305">Yoshi</a>
       </footer>
     </div>
   )
