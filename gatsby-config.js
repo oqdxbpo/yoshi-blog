641dbf66df2627c413e91d3d6561400615ec27e9
yoshi <hiryu1974@yahoo.co.jp>
2021-07-03 15:01:37 +0900

    bio　CSS補正

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `よっしーブログ`,
+    title: `YOSHI BLOG`,
     author: {
       name: `Yoshi`,
       summary: `who lives and works in San Francisco building useful things.`,
